9e58885e070fc19741265c5cdc9381aeb70bf28b
Your Name <gonzalaz@live.com.ar>
2022-10-05 14:24:22 -0300

    tienda online

---
+++ index.js
@@ -0,0 +1,9 @@
+const productos=[
+    {id:1,nombre:"Smart TV Philips 50 4K Android gris",precio:"100000",stock:"9"},
+    {id:2,nombre:"Monitor led 32 4K",precio:"26560",stock:"5"},
+    {id:3,nombre:"Smart TV Philips 50 Android gris",precio:"65999",stock:"13"},
+    {id:4,nombre:"Smart TV Samsung 65 Android",precio:"90000",stock:"25"},
+    {id:5,nombre:"monitor UHD Philips 32 gris",precio:"33267",stock:"15"},
+    {id:6,nombre:"TV SAMSUNG 72 4K Android",precio:"75000",stock:"10"},
+];
+let carrito=[]
---
+++ javaScript/app.js
@@ -0,0 +1,9 @@
+
+let products=[];
+let total=0;
+function add(product, price) {
+    console.log ( product ,  price ) ;
+    products.push(product);
+    total  =  total  +  price ;
+    document.getElementById("checkout").innerHTML=`Pagar $ ${ total } `
+}
