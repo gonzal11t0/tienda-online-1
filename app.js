65b29dd726de5a874955b56963dfb4506ee7b89f
Gonzalo Lazarte <gonzalaz@live.com.ar>
2022-10-08 17:47:34 -0300

    media desde 600 a 750px completada hasta monitores

--- app.js
+++
@@ -1,9 +0,0 @@
-
-let products=[];
-let total=0;
-function add(product, price) {
-    console.log ( product ,  price ) ;
-    products.push(product);
-    total  =  total  +  price ;
-    document.getElementById("checkout").innerHTML=`Pagar $ ${ total } `
-}
